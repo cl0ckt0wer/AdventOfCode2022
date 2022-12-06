9a3f09e67c74dfdb59d88b1923d1e8e76d02f494
Triplett, Jason <jason.triplett@igt.com>
2022-12-06 05:29:25 -0800

    Simplified

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -1,36 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-var input = File.ReadAllLines("input.txt");
-var answer = input.Select(x => GetStartIndex(x, 0)).Sum();
-var part2answer = GetMessageStart(input[0]);
+﻿var part1Answer = GetFirstUniqueSubstring(File.ReadAllLines("input.txt")[0], 4);
+var part2Answer = GetFirstUniqueSubstring(File.ReadAllLines("input.txt")[0], 14);
+Console.WriteLine($"The first part answer is: {part1Answer}. The second part answer is: {part2Answer}");
 
-int GetMessageStart(string v)
+int GetFirstUniqueSubstring(string messageString, int uniqueLength, int result = 0)
 {
-    var i = 0;
     while (true)
     {
-        if (v.Substring(i, 14).Distinct().Count() == 14) break;
-        i++;
+        if (messageString.Substring(result, uniqueLength).Distinct().Count() == uniqueLength) break;
+        result++;
     }
-    //Console.WriteLine(v.Substring(i, 14));
-    return i + 14;
-
-}
-
-Console.WriteLine($"Part 1:{answer}");
-Console.WriteLine($"Part 2: {part2answer}");
-
-static int GetStartIndex(string input, int v)
-{
-    if (input[v] != input[v + 1] && input[v] != input[v + 2] && input[v] != input[v + 3])
-    {
-        if (input[v + 1] != input[v + 2] && input[v+1] != input[v + 3])
-        {
-            if (input[v + 2] != input[v + 3])
-            {
-                return v + 4;
-            }
-        }
-    }
-    return GetStartIndex(input, v + 1);
-    
+    return result + uniqueLength;
 }
